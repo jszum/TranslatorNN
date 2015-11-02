76c118b36e10adbf8f2af0bebcff542b5c4bae9d
jszum <jaroslaw.szumega@gmail.com>
2015-11-02 11:53:20 -0800

    Object-oriented approach. Own class for storing neural network.

--- network.py
+++ network.py
@@ -3,25 +3,50 @@
 from pybrain.structure import FeedForwardNetwork
 from pybrain.structure import FullConnection
 from pybrain.structure import LinearLayer, SigmoidLayer
+from pybrain.tools.xml.networkwriter import NetworkWriter
+from pybrain.tools.xml.networkreader import NetworkReader
+
+class MyNet:
+
+	def __init__(self, file='config.xml'):
+		self.net = FeedForwardNetwork()
+		self.file = file
+
+
+	def constructNet(self, input, hidden, output): 
+		inputLayer = LinearLayer(input)
+		hiddenLayer = SigmoidLayer(hidden)
+		outputLayer = LinearLayer(output)
+
+		self.net.addInputModule(inputLayer)
+		self.net.addModule(hiddenLayer)
+		self.net.addOutputModule(outputLayer)
+
+		conn1 = FullConnection(inputLayer, hiddenLayer)
+		conn2 = FullConnection(hiddenLayer, outputLayer)
+
+		self.net.addConnection(conn1)
+		self.net.addConnection(conn2)
+
+	
+	def setup(self):
+		self.net.sortModules()
+
+	
+	def saveToFile(self):
+		NetworkWriter.writeToFile(self.net, 'config.xml')
+
+
+	def loadFromFile(self):
+		self.net = NetworkReader.readFrom('config.xml')
 
 
 if __name__ == "__main__":
 
+	network = MyNet()
+	network.constructNet(144,10,4)
+	network.setup()
 
-	net = FeedForwardNetwork()
+	network.saveToFile()
 
-	inputLayer = LinearLayer(144)
-	hiddenLayer = SigmoidLayer(10)
-	outputLayer = LinearLayer(4)
-
-	net.addInputModule(inputLayer)
-	net.addModule(hiddenLayer)
-	net.addOutputModule(outputLayer)
-
-	conn1 = FullConnection(inputLayer, hiddenLayer)
-	conn2 = FullConnection(hiddenLayer, outputLayer)
-
-	net.addConnection(conn1)
-	net.addConnection(conn2)
-
-	net.sortModules()
+	
