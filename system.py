9c2296cdbfb3a5fbb99c80654a1b3368c7035640
jszum <jaroslaw.szumega@gmail.com>
2015-11-22 14:32:02 +0100

    Result normalization and classification added.

--- system.py
+++ system.py
@@ -5,6 +5,44 @@ import network
 import sys
 import numpy as np
 
+morse_codes = {
+	"1000":"b",
+	"1010":"c",
+	"0010":"f",
+	"0000":"h",
+	"0111":"j",
+	"0100":"l",
+	"0110":"p",
+	"1101":"q",
+	"0001":"v",
+	"1001":"x",
+	"1011":"y",
+	"1100":"z"
+}
+
+def normalize(res):
+	normalized = []
+
+	for e in res:
+		if e >= 0.7:
+			normalized.append(1)
+		if e <= 0.3:
+			normalized.append(0)
+
+	return normalized
+
+def clasify(n):
+	print n
+	string = ""
+	for e in n:
+		string += str(e)
+
+	try:
+		print "qualified as letter: " + morse_codes[string]
+	except KeyError, e:
+		print "Character not recognized"
+
+
 if __name__=="__main__":
 
 	np.set_printoptions(precision=2, suppress=True)
@@ -21,6 +59,12 @@ if __name__=="__main__":
 	netw.loadFromFile(net_file)
 	result = netw.net.activate(data)
 
-	print result
+	normalized = normalize(result)
 
+	if len(normalized) == 4:
+		print result
+		clasify(normalized)
+	
+	else:
+		print "Answer not recognized"
 
